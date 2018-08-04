9662e4bd818af060c1d72e77f31c6cf77152aba0
7sena <40254036+7sena@users.noreply.github.com>
2018-08-04 14:20:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
-});
-client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag} !`);
           client.user.setActivity("Bot By TEAM FoXBot",{type: 'WATCHING'});
   
