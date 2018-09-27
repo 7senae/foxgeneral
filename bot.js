9e76906ab5e57534a5250afab4e7f462b7536aa8
7sena <40254036+7sena@users.noreply.github.com>
2018-09-27 22:25:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,7 +42,7 @@ client.user.setStatus("dnd")
 
 client.on("message", (message) => {
     /// ALPHA CODES
-   if (message.content.startsWith("-ticket")) {     /// ALPHA CODES
+   if (message.content.startsWith("$ont")) {     /// ALPHA CODES
         const reason = message.content.split(" ").slice(1).join(" ");     /// ALPHA CODES
         if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
         if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
@@ -73,7 +73,7 @@ client.on("message", (message) => {
     }
  
  
-  if (message.content.startsWith("-close")) {
+  if (message.content.startsWith("$offt")) {
         if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
  
        message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`#confirm\`. This will time out in 10 seconds and be cancelled.`)
