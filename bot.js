0ba3dc46fa9c2639323b9831bb5dc91f1b91b04b
7sena <40254036+7sena@users.noreply.github.com>
2018-09-27 22:22:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,20 +40,19 @@ client.user.setGame(`$help || $inv `,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
-
 client.on("message", (message) => {
-    /// Fox Bot
-   if (message.content.startsWith("$ont")) {     /// FoxBot
-        const reason = message.content.split(" ").slice(1).join(" ");     /// FoxBot
+    /// ALPHA CODES
+   if (message.content.startsWith("-ticket")) {     /// ALPHA CODES
+        const reason = message.content.split(" ").slice(1).join(" ");     /// ALPHA CODES
         if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
         if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
         message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
             let role = message.guild.roles.find("name", "Support Team");
             let role2 = message.guild.roles.find("name", "@everyone");
-            c.overwritePermissions(role, {  /// FoxBot
+            c.overwritePermissions(role, {
                 SEND_MESSAGES: true,
                 READ_MESSAGES: true
-            });     /// FoxBot
+            });    /// ALPHA CODES
             c.overwritePermissions(role2, {
                 SEND_MESSAGES: false,
                 READ_MESSAGES: false
@@ -65,7 +64,7 @@ client.on("message", (message) => {
             message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
             const embed = new Discord.RichEmbed()
                 .setColor(0xCF40FA)
-                .addField(`Hey ${message.author.username}!`, `يرجى محاولة شرح سبب فتح هذه التذكرة بأكبر قدر ممكن من التفاصيل. موظفينا ** ** سيكونون هنا.`)
+                .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Staff** will be here soon to help.`)
                 .setTimestamp();
             c.send({
                 embed: embed
@@ -74,19 +73,19 @@ client.on("message", (message) => {
     }
  
  
-  if (message.content.startsWith("$offt")) {  /// FoxBot
+  if (message.content.startsWith("-close")) {
         if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
  
-       message.channel.send(`هل أنت واثق؟ بعد التأكيد ، لا يمكنك عكس هذا الإجراء! \ n للتأكيد ، اكتب \ `#confirm \`. سوف ينتهي المهلة خلال 10 ثوانٍ ويتم إلغاؤها.`)
+       message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`#confirm\`. This will time out in 10 seconds and be cancelled.`)
            .then((m) => {
                message.channel.awaitMessages(response => response.content === '-confirm', {
                        max: 1,
                        time: 10000,
                        errors: ['time'],
-                   })    /// Fox Bot
+                   })    /// ALPHA CODES
                    .then((collected) => {
                        message.channel.delete();
-                   })    /// FOX BOT
+                   })    /// ALPHA CODES
                    .catch(() => {
                        m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
                            m2.delete();
